5ee83d03ec15fdc5c165cc8bfc425e0bc9380646
Alamin <alamin35-449@diu.edu.bd>
2023-03-24 02:37:08 +0600

    Initial commit

---
+++ lib/input_pages/input_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "BMI CALCULATOR",
+          style: TextStyle(color: Colors.lightGreenAccent),
+        ),
+        centerTitle: true,
+        leading: Icon(
+          Icons.view_list_outlined,
+          color: Colors.lightGreenAccent,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreen,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: const [
+                      Icon(Icons.male,size: 24,),
+                      Text("MALe",style: TextStyle(
+                        fontSize: 24,
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreen,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+
+                ),
+              ),
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bmi_calculator/input_pages/input_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget { //extends holo
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: InputPage(),
+    );
+  }
+}
